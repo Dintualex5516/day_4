d7ecccabe264d6c7ce13ee318e38abdc698300fe
Dintualex5516 <104342556+Dintualex5516@users.noreply.github.com>
2023-12-18 12:11:45 +0000

    angularweb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./units/navbar/navbar.component";
 import { DistrictsComponent } from "./pages/districts/districts.component";
+import { FooterComponent } from "./units/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'day4';
